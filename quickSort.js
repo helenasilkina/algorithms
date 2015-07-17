0c345cb8061fab20b7459ebe5960104564a0b632
Helena Silkina <helenasilkina@gmail.com>
2015-07-17 12:51:33 +0300

    Update quickSort.js

--- quickSort.js
+++ quickSort.js
@@ -1,8 +1,7 @@
-//sorting Hoar
-// native Array.sort
-
 /**
- *
+ * Sorting Hoare
+ * in javascript develop as native Array.sort
+ * 
  * Tests with:
  *
  * var array = [];
@@ -18,13 +17,70 @@ var Quicksort = (function() {
   
   /**
    * Swaps two values in the heap
-   * @param {int} indexA Index of the first item to be swapped
-   * @param {int} indexB Index of the second item to be swapped
+   * @param {int} first Index of the first item
+   * @param {int} second Index of the second item
    */
-  function swap(array, indexA, indexB) {
-    array[indexA] = array[indexB] + array[indexA];
-    array[indexB] = array[indexA] - array[indexB];
-    array[indexA] = array[indexA] - array[indexB];
+  function swap(array, first, second) {
+    array[first] = array[second] + array[first];
+    array[second] = array[first] - array[second];
+    array[first] = array[first] - array[second];
+  }
+  
+  /**
+   * Partitions the (sub)array into values less than and greater than the pivot value
+   * @param {Array} array The target array
+   * @param {int} pivot The index of the pivot
+   * @param {int} left The index of the leftmost element
+   * @param {int} left The index of the rightmost element
+   */
+  function partition(array, pivot, left, right) {
+    var startIndex = left;
+    swap(array, pivot, right);
+    for(var v = left; v < right; v++) {
+      if(array[v] < array[pivot]) {
+        swap(array, v, startIndex);
+        startIndex++;
+      }
+    }
+    swap(array, right, startIndex);
+    return startIndex;
+  }
+ 
+  /**
+   * Sort array
+   * @param {Array} array The target array
+   * @param {int} left The index of the leftmost element, defaults 0
+   * @param {int} right The index of the rightmost element, defaults array.length-1
+   */
+  function sort(array, left, right) {
+ 
+    var pivot = null;
+ 
+    if(typeof left !== 'number') {
+      left = 0;
+    }
+ 
+    if(typeof right !== 'number') {
+      right = array.length - 1;
+    }
+ 
+    // effectively set our base
+    // case here. When left == right
+    // we'll stop
+    if(left < right) {
+ 
+      // pick a pivot between left and right
+      // and update it once we've partitioned
+      // the array to values < than or > than
+      // the pivot value
+      pivot     = left + Math.ceil((right - left) * 0.5);
+      newPivot  = partition(array, pivot, left, right);
+ 
+      // recursively sort to the left and right
+      sort(array, left, newPivot - 1);
+      sort(array, newPivot + 1, right);
+    }
+ 
   }
 
 
