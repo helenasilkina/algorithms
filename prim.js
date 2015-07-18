777357f60a0211188ec1d3a9a32ffe6d4337180b
Helena Silkina <helenasilkina@gmail.com>
2015-07-18 12:37:13 +0300

    Create prim.js

---
+++ prim.js
@@ -0,0 +1,3 @@
+// https://www.bitcarpentry.com/prims-algorithm-in-js-and-clojurescript/
+// see graph javascript library https://github.com/devenbhooshan/graph.js
+// https://gist.github.com/methodin/1577481
