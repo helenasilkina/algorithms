294aec159e7a15f78aecae8e29d4f8f2fb98fad5
Helena Silkina <helenasilkina@gmail.com>
2015-07-20 10:19:15 +0300

    Update prim.js

--- prim.js
+++ prim.js
@@ -1,3 +1,12 @@
 // https://www.bitcarpentry.com/prims-algorithm-in-js-and-clojurescript/
 // see graph javascript library https://github.com/devenbhooshan/graph.js
 // https://gist.github.com/methodin/1577481
+
+/**
+ * Prim algorithm
+ * 
+ * Tests with:
+ *
+ */
+
+"use strict";
