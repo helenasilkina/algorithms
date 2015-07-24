58a8b795b5e1871cf25df1fc1fadc309ef7ab2d9
Helena Silkina <helenasilkina@gmail.com>
2015-07-24 13:22:02 +0300

    Update Prim.js

--- Prim.js
+++ Prim.js
@@ -1,12 +1,43 @@
-// https://www.bitcarpentry.com/prims-algorithm-in-js-and-clojurescript/
-// see graph javascript library https://github.com/devenbhooshan/graph.js
 // https://gist.github.com/methodin/1577481
 
 /**
  * Prim algorithm
  * 
  * Tests with:
- *
+ * var g = new Graph();
+ * g.addNode('a');
+ * g.addNode('b');
+ * g.addNode('c');
+ * g.addNode('d');
+ * g.addNode('e');
+ * g.addNode('f');
+ * g.addEdge('a','b',1);
+ * g.addEdge('b','c',3);
+ * g.addEdge('a','d',3);
+ * g.addEdge('b','d',2);
+ * g.addEdge('d','e',3);
+ * var result = Prim(g);
  */
 
 "use strict";
+var Graph = function() {
+    this.edges = {};
+    this.nodes = [];
+    this.nodeMap = {};
+    this.addNode = function(node) {
+      console.log('Node add');
+    };
+    this.addEdge = function(source, sink, capacity) {
+      console.log('Edge add');
+    };
+    this.edgeExists = function(source, sink) {
+      console.log('Edge is exist');
+    };
+};
+function Prim(graph) {
+    var result = [];
+    
+    console.log(result);
+    return result;
+};
+
