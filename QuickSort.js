0308f73a422529b34143baf33d3918fa8e255768
Helena Silkina <helenasilkina@gmail.com>
2015-08-25 09:06:00 +0300

    Update QuickSort.js

--- QuickSort.js
+++ QuickSort.js
@@ -19,16 +19,10 @@ var Quicksort = (function() {
     array[first] = array[first] - array[second];
   }
   
-  /**
-   * Partitions the (sub)array into values less than and greater than the pivot value
-   * @param {Array} array The target array
-   * @param {int} pivot The index of the pivot
-   * @param {int} left The index of the leftmost element
-   * @param {int} left The index of the rightmost element
-   */
   function partition(array, pivot, left, right) {
     var startIndex = left;
     swap(array, pivot, right);
+    
     for (var v = left; v < right; v++) {
       if(array[v] < array[pivot]) {
         swap(array, v, startIndex);
@@ -39,12 +33,6 @@ var Quicksort = (function() {
     return startIndex;
   }
  
-  /**
-   * Sort array
-   * @param {Array} array The target array
-   * @param {int} left The index of the leftmost element, defaults 0
-   * @param {int} right The index of the rightmost element, defaults array.length-1
-   */
   function sort(array, left, right) {
     left = typeof left === 'number' ? left : 0;
     right = typeof right === 'number' ?  right : array.length - 1;
@@ -54,8 +42,7 @@ var Quicksort = (function() {
       sort(array, pivot + 1, right);
     }
   }
-return {
+  return {
     sort: sort
   };
- 
 })();
