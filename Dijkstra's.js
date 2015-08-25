b586a6eb6a55783b7277fe1e65716fe4955f99ed
Helena Silkina <helenasilkina@gmail.com>
2015-08-25 09:35:50 +0300

    Update Dijkstra's.js

--- Dijkstra's.js
+++ Dijkstra's.js
@@ -21,4 +21,30 @@ function Dijkstra(Graph, source):
                   prev[v] ← u 
 
       return dist[], prev[]
+      
+      
+  Using a priority queue
+  function Dijkstra(Graph, source):
+      dist[source] ← 0                                    // Initialization
+
+      create vertex set Q
+
+      for each vertex v in Graph:           
+          if v ≠ source
+              dist[v] ← INFINITY                          // Unknown distance from source to v
+              prev[v] ← UNDEFINED                         // Predecessor of v
+
+         Q.add_with_priority(v, dist[v])
+
+
+      while Q is not empty:                              // The main loop
+         u ← Q.extract_min()                            // Remove and return best vertex
+         for each neighbor v of u:                       // only v that is still in Q
+             alt = dist[u] + length(u, v) 
+             if alt < dist[v]
+                 dist[v] ← alt
+                 prev[v] ← u
+                 Q.decrease_priority(v, alt)
+
+     return dist[], prev[]
 */
